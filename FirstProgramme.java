d41e2ee2e3ee71da88bc52deed41a3b0bb13f937
Param <param1999mangra@gmail.com>
2026-04-16 22:50:06 +0530

    first commit

---
+++ FirstProgramme.java
@@ -0,0 +1,10 @@
+public class FirstProgramme {
+   static char c = 5;
+   static float f =  8.5f;
+    public static void main(String[] args) {
+        System.out.println(c);
+       
+
+    }
+    
+}
