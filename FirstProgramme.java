f9cb769a83b33ceb848d2c018a4d17a6591d2f6d
Param <param1999mangra@gmail.com>
2026-04-18 05:57:56 +0530

    18 april

--- FirstProgramme.java
+++ FirstProgramme.java
@@ -1,9 +1,16 @@
 public class FirstProgramme {
-   static char c = 5;
-   static float f =  8.5f;
-    public static void main(String[] args) {
-        System.out.println(c);
-       
+static  int a = 5;
+      public static void main(String[] args) {
+    //  Integer obj = Integer.valueOf(a);
+    Integer obj = a;
+    int a = 1;
+    //  int a = obj.intValue();
+        // System.out.println(args[1]);
+      
+        short s = (short) 33000;
+        System.out.println(++a);
+        System.out.println(a++);
+
 
     }
     
