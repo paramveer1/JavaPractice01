0e98824690634e08a8e0196cf03b8ebfe2fc177b
Param <param1999mangra@gmail.com>
2026-04-26 14:08:20 +0530

    26 april

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -4,11 +4,10 @@ import UpperFolder.CalcUsingMethod;
 public class Demo {
     
     public static void main(String[] args) {
-        int a = 0b101;
-        CalcUsingMethod calc = new CalcUsingMethod();
-        System.out.println(        calc.addition(8, 9)
-);
-        // System.out.println(a);
+        byte a = 0b101;
+        
+
+        System.out.println(a);
     }
     
 }
---
+++ NestedIfElse.java
@@ -0,0 +1,49 @@
+import java .util.Scanner;
+public class NestedIfElse {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Admissions in Girls school \nPlease Enter Your Details");
+        System.out.println("1.Enter your Percentage ");
+        Scanner sc = new Scanner(System.in);
+        int percentage = sc.nextInt();
+        System.out.println("Enter Your Gender m/f ");
+        char gender =  sc.next().charAt(0);
+        System.out.println("Enter Income of your father");
+        int income = sc.nextInt();
+        if(percentage>50){
+            if(income>50000){
+                if(gender == 'f'|| gender == 'F'){
+                    System.out.println("Congo! You are Eligible");
+                }
+                else{
+                System.out.println("Not  : Only girls allowed");
+                }
+            }
+            else{
+
+                if(gender!= 'f' || gender != 'F')
+                System.out.println("Not Eligible : Income is low also only girls allowed");
+            }
+
+        }
+        else{
+            if(income<50000 && (gender != 'F' || gender != 'f') )
+            {
+                System.out.println( " percentage is low , Income is less , only Girls allowed here");
+                return;
+
+            }
+            if(income<50000){
+            System.out.println("Not Eligible : Percentage and Income are low ");
+            return;
+        }
+        if(gender != 'F' || gender != 'f'){
+            System.out.println("Not Eligible : Percentage and gender is not appropriate");
+            return;
+        }
+      System.out.println("Percentage is low");
+
+
+    }
+    
+}
+}
