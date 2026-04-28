3daec7bcfadbfd459278550caca89b8593de3e0e
Param <param1999mangra@gmail.com>
2026-04-28 23:31:47 +0530

    28april

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -4,10 +4,11 @@ import UpperFolder.CalcUsingMethod;
 public class Demo {
     
     public static void main(String[] args) {
-        byte a = 0b101;
+        int a = 45;
+       char ch = 98;
         
-
-        System.out.println(a);
+      
+        System.out.println(a>ch?a:ch);
     }
     
 }
