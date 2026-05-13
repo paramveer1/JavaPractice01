e88611cad6e7f29ba6e490e24e9c0e909cee8556
Param <param1999mangra@gmail.com>
2026-05-13 07:13:58 +0530

    ok

--- MultiThreading/Ok.java
+++ MultiThreading/Ok.java
@@ -2,13 +2,7 @@ package MultiThreading;
 
 
     public class Ok {
-    public static void main(String[] args) {
-        Table table = new Table();
-                Thread thread = new Thread(table);
-                thread.start();
-
-        
-    }
+   
     
 }
     
--- MultiThreading/RunnableOneClassMultiplethread.java
+++ MultiThreading/RunnableOneClassMultiplethread.java
@@ -2,9 +2,34 @@ package MultiThreading;
 
 
 
-class Table implements Runnable{
+public class RunnableOneClassMultiplethread implements Runnable{
+   
+
+     public static void main(String[] args) {
+        RunnableOneClassMultiplethread table = new RunnableOneClassMultiplethread();
+        //         RunnableOneClassMultiplethread table2 = new RunnableOneClassMultiplethread();
+        // RunnableOneClassMultiplethread table3 = new RunnableOneClassMultiplethread();
+
+                Thread thread1 = new Thread(table);
+                Thread thread2 = new Thread(table);
+                Thread thread3 = new Thread(table);
+
+                // thread1.setName("Thread1");
+                thread1.start();
+                                thread2.start();
+                                                thread3.start();
+
+
+
+        
+    }
     public void run(){
-        System.out.println("Thread task is being executed");
+         String name = Thread.currentThread().getName();
+        if(name.equals("Five")){
+            
+
+        }
+        System.out.println(" Thread task is being executedd");
     }
 
     
@@ -12,4 +37,3 @@ class Table implements Runnable{
 
 }
 
-    
