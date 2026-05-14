ab4b1fe7c68e24ee7c47aa5d4176ab355e2c309b
Param <param1999mangra@gmail.com>
2026-05-14 07:23:55 +0530

    14may

--- MultiThreading/RunnableOneClassMultiplethread.java
+++ MultiThreading/RunnableOneClassMultiplethread.java
@@ -5,7 +5,7 @@ package MultiThreading;
 public class RunnableOneClassMultiplethread implements Runnable{
    
 
-     public static void main(String[] args) {
+     public static void main(String[] args)  {
         RunnableOneClassMultiplethread table = new RunnableOneClassMultiplethread();
         //         RunnableOneClassMultiplethread table2 = new RunnableOneClassMultiplethread();
         // RunnableOneClassMultiplethread table3 = new RunnableOneClassMultiplethread();
@@ -13,6 +13,8 @@ public class RunnableOneClassMultiplethread implements Runnable{
                 Thread thread1 = new Thread(table);
                 Thread thread2 = new Thread(table);
                 Thread thread3 = new Thread(table);
+                thread1.setPriority(10);
+                thread3.setPriority(1);
 
                 // thread1.setName("Thread1");
                 thread1.start();
@@ -25,11 +27,39 @@ public class RunnableOneClassMultiplethread implements Runnable{
     }
     public void run(){
          String name = Thread.currentThread().getName();
-        if(name.equals("Five")){
+        if(name.equals("Thread-0")){
+            tablePrint(2);
             
 
         }
-        System.out.println(" Thread task is being executedd");
+
+         if(name.equals("Thread-1")){
+            tablePrint(3);
+            
+
+        }
+         if(name.equals("Thread-2")){
+            tablePrint(4);
+            
+
+        }
+                  
+
+
+        
+    }
+
+    void tablePrint(int n) {
+        for(int i = 1;i<= 10;i++){
+            System.out.println(i*n+ " "+ Thread.currentThread().getName());
+        }
+        try{
+        Thread.sleep(500);
+        }
+        catch(InterruptedException e){
+            System.out.println("Exception Handled");
+        }
+
     }
 
     
