691d333c937418a212050d842b7b1cb6844d2a23
Param <param1999mangra@gmail.com>
2026-07-04 07:53:47 +0530

    4july

--- FileIO/CreateFile.java
+++ FileIO/CreateFile.java
@@ -1,5 +1,7 @@
 package FileIO;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -12,8 +14,35 @@ public class CreateFile {
 
     //    }
         // file.delete();
+        try{ 
+            File file = new File("c:/users/pc/desktop/fileio/file1");
+            file.createNewFile();
+        FileOutputStream fos = new FileOutputStream("c:/users/pc/desktop/fileio/file1");
+        FileInputStream fis = new FileInputStream("c:/users/pc/desktop/fileio/file1");
+        String s = "param";
+        // byte[] b = ;
+        
+         fos.write(s.getBytes());
+         System.out.println("File Written");
+         
+         while (true){
+            int n = fis.read();
+            if(n==-1)break;
+          
+            System.out.print((char)n);
+            
 
-        FileOutputStream fos = new FileOutputStream()        );
+         }
+        
+
+
+        }
+        catch(IOException e){
+            System.out.println("File Not Found Exception");
+
+        }
+       
+
     }
     
 }
