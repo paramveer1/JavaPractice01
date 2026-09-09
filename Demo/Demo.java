76138174abf593e08aa2a29eed24be70d0aeb551
Param <param1999mangra@gmail.com>
2026-09-09 22:32:14 +0530

    9sep

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -4,10 +4,29 @@ package Demo;
  public class Demo{
   
     public static void main(String[] args) {
-      System.out.println(4/0);
+      Child p = new Child();
+      p.marry();
+      
+
+      
     
        
        
     }
     
+ }
+
+ class Parent {
+   int marry(){
+    System.out.println("marry sheela");
+    return 0;
+
+  }
+ }
+
+ class Child extends  Parent{
+ void show(){
+  System.out.println("in child");
+ }
+
  }
---
+++ Demo/Vehicle.java
@@ -0,0 +1,7 @@
+package Demo;
+
+public interface Vehicle {
+    public static final int a;
+    
+    
+}
